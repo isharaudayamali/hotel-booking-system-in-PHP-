21fd609be7ebe8ea2a2ad6fd4f33b327e2346aed
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-09-17 22:38:10 +0530

    some error fixed

--- auth/login.php
+++ auth/login.php
@@ -34,7 +34,7 @@ if (isset($_POST['submit'])) {
 
 ?>
 
-<div class="hero-wrap js-fullheight" style="background-image: url('images/image_2.jpg');" data-stellar-background-ratio="0.5">
+<div class="hero-wrap js-fullheight" style="background-image: url('<?php echo APPURL;?> images/image_2.jpg');" data-stellar-background-ratio="0.5">
   <div class="overlay"></div>
   <div class="container">
     <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
