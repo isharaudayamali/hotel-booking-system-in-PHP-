699c15b375082907edcf609a39875858677df8b8
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-10-09 11:40:40 +0530

    single room finalized

--- index.php
+++ index.php
@@ -66,14 +66,14 @@
     						<div class="text p-4 p-xl-5 text-center">
     							<p class="star mb-0"><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></p>
     							<!-- <p class="mb-0"><span class="price mr-1">$120.00</span> <span class="per">per night</span></p> -->
-	    						<h3 class="mb-3"><a href="room-single.php?id=<?php echo $room->id; ?>"><?php echo $room->name; ?></a></h3>
+	    						<h3 class="mb-3"><a href="<?php echo APPURL;?>/rooms/room-single.php?id=<?php echo $room->id; ?>"><?php echo $room->name; ?></a></h3>
 	    						<ul class="list-accomodation">
 	    							<li><span>Max:</span><?php echo $room->num_persons; ?></li>
 	    							<li><span>Size:</span><?php echo $room->size; ?></li>
 	    							<li><span>View:</span> <?php echo $room->view; ?></li>
 	    							<li><span>Bed:</span> <?php echo $room->num_beds; ?></li>
 	    						</ul>
-	    						<p class="pt-1"><a href="room-single.php?id=<?php echo $room->id; ?>" class="btn-custom px-3 py-2">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+	    						<p class="pt-1"><a href="<?php echo APPURL;?>/rooms/room-single.php?id=<?php echo $room->id; ?>" class="btn-custom px-3 py-2">View Room Details <span class="icon-long-arrow-right"></span></a></p>
     						</div>
     					</div>
     				</div>
