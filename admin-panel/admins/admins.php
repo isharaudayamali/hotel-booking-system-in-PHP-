41429411654d80aad2e77863d91b683006be5dfd
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-10-20 12:39:50 +0530

    showing_admin

--- admin-panel/admins/admins.php
+++ admin-panel/admins/admins.php
@@ -1,66 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
-    <title>Admin Panel</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-     <link href="../styles/style.css" rel="stylesheet">
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div id="wrapper">
-    <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
-      <div class="container">
-      <a class="navbar-brand" href="#">LOGO</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<?php require '../layouts/header.php'; ?>
+<?php require "../../config/config.php" ?>
 
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav side-nav" >
-          <li class="nav-item">
-            <a class="nav-link" style="margin-left: 20px;" href="../index.html">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="admins.html" style="margin-left: 20px;">Admins</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../hotels-admins/show-hotels.html" style="margin-left: 20px;">Hotels</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../rooms-admins/show-rooms.html" style="margin-left: 20px;">Rooms</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../bookings-admins/show-bookings.html" style="margin-left: 20px;">Bookings</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-md-auto d-md-flex">
-          <li class="nav-item">
-            <a class="nav-link" href="../index.html">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              username
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Logout</a>
-              
-          </li>
-                          
-          
-        </ul>
-      </div>
-    </div>
-    </nav>
-    <div class="container-fluid">
+<?php 
+
+if (!isset($_SESSION['adminname'])) {
+  echo "<script>window.location.href='" . ADMINURL . "admins/login-admins.php';</script>";
+}
+
+$admins = $conn->query("SELECT * FROM admins");
+$admins->execute();
+
+$allAdmins = $admins->fetchAll(PDO::FETCH_OBJ);
+
+?>
 
       <div class="row">
         <div class="col">
@@ -72,29 +24,26 @@
                 <thead>
                   <tr>
                     <th scope="col">#</th>
-                    <th scope="col">username</th>
+                    <th scope="col">allAdmins</th>
                     <th scope="col">email</th>
                   </tr>
                 </thead>
                 <tbody>
+                  <?php foreach($allAdmins as $admin): ?>
                   <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                   
+                    <th scope="row"><?php echo $admin->id; ?></th>
+                    <td><?php echo $admin->adminname; ?></td>
+                    <td><?php echo $admin->email; ?></td>
+
                   </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                   
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                   
-                  </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table> 
+            </div>
+          </div>
+        </div>
+      </div>
+
                 </tbody>
               </table> 
             </div>
