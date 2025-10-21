94a529c78452913caaf03d78e389c6be05a01256
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-10-21 11:22:36 +0530

    show_booking

---
+++ admin-panel/bookings-admins/show-bookings.php
@@ -0,0 +1,66 @@
+<?php require '../layouts/header.php'; ?>
+<?php require "../../config/config.php" ?>
+
+<?php
+
+  if (!isset($_SESSION['adminname'])) {
+    echo "<script>window.location.href='" . ADMINURL . "admins/login-admins.php';</script>";
+  }
+
+  $bookings = $conn->query("SELECT * FROM bookings");
+  $bookings->execute();
+
+  $allBookings = $bookings->fetchAll(PDO::FETCH_OBJ);
+?>
+
+          <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Bookings</h5>
+            
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">check in</th>
+                    <th scope="col">check out</th>
+                    <th scope="col">email</th>
+                    <th scope="col">phone number</th>
+                    <th scope="col">full name</th>
+                    <th scope="col">hotel name</th>
+                    <th scope="col">room name</th>
+                    <th scope="col">status</th>
+                    <th scope="col">payment</th>
+                    <th scope="col">created at</th>
+                    <th scope="col">update</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach($allBookings as $index => $booking): ?>
+                  <tr>
+                    <th scope="row"><?php echo $booking->id; ?></th>
+                    <td><?php echo $booking->check_in; ?></td>
+                    <td><?php echo $booking->check_out; ?></td>
+                    <td><?php echo $booking->email; ?></td>
+                    <td><?php echo $booking->phone_number; ?></td>
+                    <td><?php echo $booking->full_name; ?></td>
+                    <td><?php echo $booking->hotel_name; ?></td>
+                    <td><?php echo $booking->room_name; ?></td>
+                    <td><?php echo $booking->status; ?></td>
+                    <td><?php echo $booking->payment; ?></td>
+                    <td><?php echo $booking->create_at; ?></td>
+
+                     <td><a href="status-booking.php?id=<?php echo $booking->id; ?>" class="btn btn-warning text-text-white text-center ">status</a></td>
+                  </tr>
+                  <?php endforeach; ?>
+
+                </tbody>
+              </table> 
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+<?php require '../layouts/footer.php'; ?>
